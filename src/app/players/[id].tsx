c70851deee0e3d79a01a34b5c7a516d7464e8397
Nick Pachulski <nick@pachulski.me>
2024-03-15 19:54:39 -0400

    wip

--- src/app/players/[id].tsx
+++ src/app/players/[id].tsx
@@ -1,20 +1,15 @@
 import React from "react"
 import * as ReactNative from "react-native"
 import * as ExpoRouter from "expo-router"
-import Dialog from "react-native-dialog"
-import * as Animatable from "react-native-animatable"
-import Config from "Config"
 import formatPhoneNumber from "helpers/formatPhoneNumber"
 import useTheme from "hooks/useTheme"
 import CenteredLoadingSpinner from "components/CenteredLoadingSpinner"
 import useTheCachedPlayerFirstOrGetThePlayerFromTheApi from "hooks/useTheCachedPlayerFirstOrGetThePlayerFromTheApi"
-import AsyncStorage from "@react-native-async-storage/async-storage"
-import NavigationHeaderToastNotification from "components/NavigationHeaderToastNotification"
-
-const completeConfirmationCodeLength = 6
+import PlayerAuthenticationFlow from "components/PlayerAuthenticationFlow"
 
 const Player = (): React.ReactElement => {
   const { id: playerId } = ExpoRouter.useLocalSearchParams()
+
   const player = useTheCachedPlayerFirstOrGetThePlayerFromTheApi(playerId)
 
   const navigationBarTitleLabel = React.useMemo(
@@ -32,68 +27,6 @@ const Player = (): React.ReactElement => {
 
   const theme = useTheme()
 
-  const { showNotification } = React.useContext(
-    NavigationHeaderToastNotification.Context,
-  )
-
-  const [confirmationCodeInputIsVisible, setConfirmationCodeDialogIsVisible] =
-    React.useState(false)
-
-  const sendTextMessageConfirmationCode = async (): Promise<void> => {
-    if (player) {
-      await fetch(
-        `${Config.apiUrl}/players/${player.id.toString()}/send_text_message_confirmation_code`,
-      )
-      setConfirmationCodeDialogIsVisible(true)
-    }
-  }
-
-  const viewRefThatAnimatesTheConfirmationCodeInputPopupWhenIncorrectCodesAreEntered =
-    React.useRef<Animatable.View>(null)
-
-  const [confirmationCode, setConfirmationCode] = React.useState("")
-
-  const removeConfirmationCodeInputPopup = React.useCallback(() => {
-    ReactNative.Keyboard.dismiss()
-    setConfirmationCodeDialogIsVisible(false)
-    setConfirmationCode("")
-  }, [])
-
-  const checkConfirmationCodeCorrectness = React.useCallback(async () => {
-    if (player) {
-      const response = await (
-        await fetch(
-          `${Config.apiUrl}/players/${player.id}/check_text_message_confirmation_code?confirmation_code=${confirmationCode}`,
-          { method: "post" },
-        )
-      ).json()
-      if (response.status === "correct") {
-        await AsyncStorage.setItem("API Token", response.apiToken)
-        showNotification({
-          type: "success",
-          message: `Hey ${player.first_name}! You're signed in.`,
-          dismissAfter: 3,
-        })
-        removeConfirmationCodeInputPopup()
-      } else {
-        if (
-          viewRefThatAnimatesTheConfirmationCodeInputPopupWhenIncorrectCodesAreEntered.current
-        ) {
-          // @ts-ignore https://github.com/oblador/react-native-animatable/issues/313#issuecomment-951038397
-          viewRefThatAnimatesTheConfirmationCodeInputPopupWhenIncorrectCodesAreEntered.current.shake(
-            300,
-          )
-        }
-        setConfirmationCode("")
-      }
-    }
-  }, [
-    confirmationCode,
-    player,
-    removeConfirmationCodeInputPopup,
-    showNotification,
-  ])
-
   return player ? (
     <>
       <ExpoRouter.Stack.Screen options={{ title: navigationBarTitleLabel }} />
@@ -110,39 +43,7 @@ const Player = (): React.ReactElement => {
           {formattedPhoneNumberLabel}
         </ReactNative.Text>
         <ReactNative.View style={{ height: 8 }} />
-        <ReactNative.Button
-          testID="This is Me Button"
-          title="👋 This is Me"
-          onPress={sendTextMessageConfirmationCode}
-        />
-        <Dialog.Container visible={confirmationCodeInputIsVisible}>
-          <Dialog.Title>Texted You 😘</Dialog.Title>
-          <Animatable.View
-            ref={
-              viewRefThatAnimatesTheConfirmationCodeInputPopupWhenIncorrectCodesAreEntered
-            }
-          >
-            <Dialog.CodeInput
-              autoFocus
-              codeLength={completeConfirmationCodeLength}
-              value={confirmationCode}
-              onChangeText={setConfirmationCode}
-              testID="Confirmation Code Input"
-            />
-          </Animatable.View>
-          <Dialog.Button
-            label="Cancel"
-            onPress={removeConfirmationCodeInputPopup}
-          />
-          <Dialog.Button
-            testID="OK Button"
-            label="OK"
-            onPress={checkConfirmationCodeCorrectness}
-            disabled={
-              confirmationCode.length !== completeConfirmationCodeLength
-            }
-          />
-        </Dialog.Container>
+        <PlayerAuthenticationFlow player={player} />
       </ReactNative.View>
     </>
   ) : (
---
+++ src/components/PlayerAuthenticationFlow.tsx
@@ -0,0 +1,113 @@
+import React from "react"
+import * as ReactNative from "react-native"
+import * as Animatable from "react-native-animatable"
+import Dialog from "react-native-dialog"
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import NavigationHeaderToastNotification from "./NavigationHeaderToastNotification"
+import type { Player } from "types/Player"
+import Config from "Config"
+
+const completeConfirmationCodeLength = 6
+
+interface PlayerAuthenticationFlowProps {
+  player: Player
+}
+
+const PlayerAuthenticationFlow = ({
+  player,
+}: PlayerAuthenticationFlowProps): React.ReactElement => {
+  const { showNotification } = React.useContext(
+    NavigationHeaderToastNotification.Context,
+  )
+
+  const [confirmationCodeInputIsVisible, setConfirmationCodeDialogIsVisible] =
+    React.useState(false)
+
+  const sendTextMessageConfirmationCode = async (): Promise<void> => {
+    setConfirmationCodeDialogIsVisible(true)
+    await fetch(
+      `${Config.apiUrl}/players/${player.id.toString()}/send_text_message_confirmation_code`,
+    )
+  }
+
+  const viewRefThatAnimatesTheConfirmationCodeInputPopupWhenIncorrectCodesAreEntered =
+    React.useRef<Animatable.View>(null)
+
+  const [confirmationCode, setConfirmationCode] = React.useState("")
+
+  const removeConfirmationCodeInputPopup = React.useCallback(() => {
+    ReactNative.Keyboard.dismiss()
+    setConfirmationCodeDialogIsVisible(false)
+    setConfirmationCode("")
+  }, [])
+
+  const checkConfirmationCodeCorrectness = React.useCallback(async () => {
+    const response = await (
+      await fetch(
+        `${Config.apiUrl}/players/${player.id}/check_text_message_confirmation_code?confirmation_code=${confirmationCode}`,
+        { method: "post" },
+      )
+    ).json()
+    if (response.status === "correct") {
+      await AsyncStorage.setItem("API Token", response.apiToken)
+      showNotification({
+        type: "success",
+        message: `Hey ${player.first_name}! You're signed in.`,
+        dismissAfter: 3,
+      })
+      removeConfirmationCodeInputPopup()
+    } else {
+      if (
+        viewRefThatAnimatesTheConfirmationCodeInputPopupWhenIncorrectCodesAreEntered.current
+      ) {
+        // @ts-ignore https://github.com/oblador/react-native-animatable/issues/313#issuecomment-951038397
+        viewRefThatAnimatesTheConfirmationCodeInputPopupWhenIncorrectCodesAreEntered.current.shake(
+          300,
+        )
+      }
+      setConfirmationCode("")
+    }
+  }, [
+    confirmationCode,
+    player,
+    removeConfirmationCodeInputPopup,
+    showNotification,
+  ])
+  return (
+    <>
+      <ReactNative.Button
+        testID="This is Me Button"
+        title="👋 This is Me"
+        onPress={sendTextMessageConfirmationCode}
+      />
+      <Dialog.Container visible={confirmationCodeInputIsVisible}>
+        <Dialog.Title>Texted You 😘</Dialog.Title>
+        <Animatable.View
+          ref={
+            viewRefThatAnimatesTheConfirmationCodeInputPopupWhenIncorrectCodesAreEntered
+          }
+        >
+          <Dialog.CodeInput
+            autoFocus
+            codeLength={completeConfirmationCodeLength}
+            value={confirmationCode}
+            onChangeText={setConfirmationCode}
+            testID="Confirmation Code Input"
+          />
+        </Animatable.View>
+        <Dialog.Button
+          label="Cancel"
+          onPress={removeConfirmationCodeInputPopup}
+        />
+        <Dialog.Button
+          testID="OK Button"
+          label="OK"
+          onPress={checkConfirmationCodeCorrectness}
+          disabled={confirmationCode.length !== completeConfirmationCodeLength}
+        />
+      </Dialog.Container>
+    </>
+  )
+}
+
+export default PlayerAuthenticationFlow
