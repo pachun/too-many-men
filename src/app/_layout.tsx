8da504cedc4a3b2b8d37732b48a13c5a9007da8b
Nick Pachulski <nick@pachulski.me>
2024-03-16 19:08:38 -0400

    wip

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,16 +2,9 @@ import React from "react"
 import * as ExpoRouter from "expo-router"
 import * as ExpoStatusBar from "expo-status-bar"
 import * as ExpoVectorIcons from "@expo/vector-icons"
-import * as ReactNavigationNative from "@react-navigation/native"
-import NavigationHeaderToastNotification from "components/NavigationHeaderToastNotification"
 import useOverTheAirUpdates from "hooks/useOverTheAirUpdates"
-import useTheme from "hooks/useTheme"
 import { initializeAptabase, trackAptabaseEvent } from "helpers/aptabase"
-import RefreshableGamesContext from "contexts/RefreshableGamesContext"
-import RefreshablePlayersContext from "contexts/RefreshablePlayersContext"
-import type { Player } from "types/Player"
-import type { RefreshableRequest } from "types/RefreshableRequest"
-import type { Game } from "types/Game"
+import Providers from "components/Providers"
 
 initializeAptabase()
 
@@ -22,69 +15,45 @@ const Layout = (): React.ReactElement => {
 
   useOverTheAirUpdates()
 
-  const theme = useTheme()
-
-  const [refreshablePlayers, setRefreshablePlayers] = React.useState<
-    RefreshableRequest<Player[]>
-  >({
-    status: "Not Started",
-  })
-
-  const [refreshableGames, setRefreshableGames] = React.useState<
-    RefreshableRequest<Game[]>
-  >({
-    status: "Not Started",
-  })
-
   return (
     <>
       <ExpoStatusBar.StatusBar style="auto" />
-      <ReactNavigationNative.ThemeProvider value={theme}>
-        <RefreshableGamesContext.Provider
-          value={{ refreshableGames, setRefreshableGames }}
-        >
-          <RefreshablePlayersContext.Provider
-            value={{ refreshablePlayers, setRefreshablePlayers }}
-          >
-            <NavigationHeaderToastNotification.Provider>
-              <ExpoRouter.Tabs screenOptions={{ headerShown: false }}>
-                <ExpoRouter.Tabs.Screen
-                  name="players"
-                  options={{
-                    title: "Team",
-                    tabBarIcon: ({ color }) => (
-                      <ExpoVectorIcons.FontAwesome6
-                        name="people-group"
-                        color={color}
-                        size={20}
-                      />
-                    ),
-                  }}
+      <Providers>
+        <ExpoRouter.Tabs screenOptions={{ headerShown: false }}>
+          <ExpoRouter.Tabs.Screen
+            name="players"
+            options={{
+              title: "Team",
+              tabBarIcon: ({ color }) => (
+                <ExpoVectorIcons.FontAwesome6
+                  name="people-group"
+                  color={color}
+                  size={20}
                 />
-                <ExpoRouter.Tabs.Screen
-                  name="games"
-                  options={{
-                    title: "Games",
-                    tabBarIcon: ({ color }) => (
-                      <ExpoVectorIcons.FontAwesome
-                        name="calendar"
-                        color={color}
-                        size={20}
-                      />
-                    ),
-                  }}
+              ),
+            }}
+          />
+          <ExpoRouter.Tabs.Screen
+            name="games"
+            options={{
+              title: "Games",
+              tabBarIcon: ({ color }) => (
+                <ExpoVectorIcons.FontAwesome
+                  name="calendar"
+                  color={color}
+                  size={20}
                 />
-                <ExpoRouter.Tabs.Screen
-                  name="index"
-                  options={{
-                    href: null,
-                  }}
-                />
-              </ExpoRouter.Tabs>
-            </NavigationHeaderToastNotification.Provider>
-          </RefreshablePlayersContext.Provider>
-        </RefreshableGamesContext.Provider>
-      </ReactNavigationNative.ThemeProvider>
+              ),
+            }}
+          />
+          <ExpoRouter.Tabs.Screen
+            name="index"
+            options={{
+              href: null,
+            }}
+          />
+        </ExpoRouter.Tabs>
+      </Providers>
     </>
   )
 }
