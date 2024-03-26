887effe4aab8e46168a63fed5628c8ea1f5d62f8
Nick Pachulski <nick@pachulski.me>
2024-03-26 00:10:01 -0400

    wip

--- src/hooks/useEffectEverySecond.ts
+++ src/hooks/useEffectEverySecond.ts
@@ -1,10 +1,13 @@
 import React from "react"
 
+const updateFrequencyInSeconds = 1
+const millisecondsInASecond = 1000
+
 const useEffectEverySecond = (effect: () => void): void => {
   React.useEffect(() => {
     const intervalId = setInterval(() => {
       effect()
-    }, 1000)
+    }, updateFrequencyInSeconds * millisecondsInASecond)
 
     effect()
 
