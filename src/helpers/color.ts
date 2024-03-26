b1c7b8812eba58c86dbd13f7b88c595753437847
Nick Pachulski <nick@pachulski.me>
2024-03-26 03:30:40 -0400

    wip

--- src/helpers/color.ts
+++ src/helpers/color.ts
@@ -22,14 +22,21 @@ type IosSystemColors =
   | "gray"
 
 const color = (color: IosSystemColors): string => {
-  const ios = `system${color.at(0)?.toUpperCase()}${color.slice(1)}`
+  const ios = `system${color.at(0)!.toUpperCase()}${color.slice(1)}`
 
   switch (color) {
+    // this hasn't been useed yet, so it isn't tested;
+    // we just want to call out that mint is the only iOS system
+    // color that RN color keywords don't support, while continuing
+    // to support the mint iOS system color with a RN alternative.
+    //
+    //* c8 ignore start */
     case "mint":
       return iosSystemColorWithOtherPlatformAlternative({
         ios,
         otherPlatforms: "lightcyan",
       })
+    //* c8 ignore end */
     default:
       return iosSystemColorWithOtherPlatformAlternative({
         ios,
