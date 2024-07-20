26894bda1af988a5e3f539c051061811b0f7e91c
Nick Pachulski <nick@pachulski.me>
2024-07-19 20:38:38 -0400

    Remove LAN IP from version control
    My personal LAN IP was in version control prior to this commit and it
    shouldn't have been. Environment variables in Expo are tough to get
    right. This commit removes my LAN IP from version control.
    
    I have a .env.local file on my machine setting EXPO_PUBLIC_API_URL to
    my lan ip. The reason I like using my LAN IP address rather than
    localhost is because my actual phone (not the simulator) can connect
    to the local server using the LAN IP address.
    
    Locally, the api url will be what's in my .env.local file (my lan ip).
    
    There's a problem with creating .env.test files for running tests with
    the jest-expo preset. It doesn't load the environment variables. So,
    there's a line in `src/Config.ts` for setting the api url in tests:
    
    `apiUrl: process.env.EXPO_PUBLIC_API_URL || "http://localhost:3000"`
    
    Then, in production, the .env file will be loaded. OTA updates load the
    environment variables too. So if we send one out locally, my lan ip will
    be used unless I delete the .env.local file first. This shouldn't be a
    problem since updates are sent in a GitHub action which only has the
    production api url in the .env file.

--- src/Config.ts
+++ src/Config.ts
@@ -1,16 +1,5 @@
-// https://docs.expo.dev/eas-update/environment-variables/
-
-import * as Updates from "expo-updates"
-
 const Config = {
-  apiUrl: process.env.EXPO_PUBLIC_API_URL || "http://192.168.1.29:3000",
-  // apiUrl: "https://too-many-men-api-8dcb4a385e6b.herokuapp.com",
-}
-
-if (Updates.channel === "production") {
-  Config.apiUrl = "https://too-many-men-api-8dcb4a385e6b.herokuapp.com"
-} else if (Updates.channel === "preview") {
-  Config.apiUrl = "https://too-many-men-api-8dcb4a385e6b.herokuapp.com"
+  apiUrl: process.env.EXPO_PUBLIC_API_URL || "http://localhost:3000",
 }
 
 export default Config
